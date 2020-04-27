4ada7202385370d9202cc414a117afb1c8b5ffa9
Mark <mark@murphymark.me>
2020-04-27 16:16:09 -0500

    env variables working

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase";
+
+const config = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASEURL,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+};
+
+firebase.initializeApp(config);
+const databaseRef = firebase.database().ref();
+export const friendsRef = databaseRef.child("friends");
