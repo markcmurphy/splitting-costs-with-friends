69c5d90dad7ac24c6ad25a9f316dddda81a8417a
markcmurphy <mark@murphymark.me>
2020-07-10 11:47:53 -0500

    Updates

---
+++ precache-manifest.279a99f3cb1ec2a4e71559d67ab8307c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bcc63a94cc33ef2d1d4f669dfe3187a",
+    "url": "/splitting-costs-with-friends/index.html"
+  },
+  {
+    "revision": "8ec19a4068a1fdb06a2a",
+    "url": "/splitting-costs-with-friends/static/css/2.326c04ff.chunk.css"
+  },
+  {
+    "revision": "ccafb3ebb5297fffd4ac",
+    "url": "/splitting-costs-with-friends/static/css/main.26299cf3.chunk.css"
+  },
+  {
+    "revision": "8ec19a4068a1fdb06a2a",
+    "url": "/splitting-costs-with-friends/static/js/2.c5ea51b1.chunk.js"
+  },
+  {
+    "revision": "af23476e578899cf4fde3e630f235c3a",
+    "url": "/splitting-costs-with-friends/static/js/2.c5ea51b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccafb3ebb5297fffd4ac",
+    "url": "/splitting-costs-with-friends/static/js/main.ff076011.chunk.js"
+  },
+  {
+    "revision": "394e2e3d238f3bafc4df",
+    "url": "/splitting-costs-with-friends/static/js/runtime-main.9be7406c.js"
+  }
+]);
